08d866d384e13ac6ea051a081432aa31115acc19
thanhhui1709 <thanhhui1709@gmail.com>
2025-09-15 14:52:33 +0700

    new map, map visualization

--- Assets/Scripts/BuildingSystem/PlacementSystem.cs
+++ Assets/Scripts/BuildingSystem/PlacementSystem.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlacementSystem : MonoBehaviour
 {
     [SerializeField]
-    private GameObject hexaIndicator;
+    private GameObject indicator;
     [SerializeField]
     private Vector3 offsetPos;
     [SerializeField]
@@ -14,9 +14,11 @@ public class PlacementSystem : MonoBehaviour
     private void Update()
     {
         Vector3 mousePos = inputManager.selectedMapPosition();
+       
         Vector3Int gridPos=grid.WorldToCell(mousePos);
        
-        hexaIndicator.transform.position=grid.GetCellCenterWorld(gridPos)+offsetPos;
+
+        indicator.transform.position=grid.GetCellCenterWorld(gridPos)+offsetPos;
        
     }
 
