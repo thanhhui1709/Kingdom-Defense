c00ec8a0f5532a3b9abdc9bd6dc3c8b0ace87b31
ndtfpt31122003thinhndhe186687 <thinhndhe186687@fpt.edu.vn>
2025-10-06 20:43:12 +0700

    tower place

---
+++ Assets/Scripts/BuildingSystem/TowerData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Tower", menuName = "Towers/Tower Data")]
+public class TowerData : ScriptableObject
+{
+    public string towerName; // Tên trụ (ví dụ: "Archer")
+    public GameObject towerPrefab; // Prefab của trụ
+    public Sprite towerIcon; // Icon để hiển thị trên nút UI
+    public int buildCost; // Giá tiền để xây
+                          // Bạn có thể thêm các thuộc tính khác như mô tả, sát thương, tầm bắn...
+}
