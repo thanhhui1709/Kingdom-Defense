e84ddb9ced2d0f92680441c7fa6b61af12aa1fad
thanhhui1709 <thanhhui1709@gmail.com>
2025-11-04 15:38:48 +0700

    fix wait scene

--- Assets/Scripts/UIManager/WaitSceneUIManager.cs
+++ Assets/Scripts/UIManager/WaitSceneUIManager.cs
@@ -7,20 +7,11 @@ public class UpdateButton
 {
     public Sprite lvImage;
     public Sprite buttonImage;
-    public 
+
 }
 public class WaitSceneUIManager : MonoBehaviour
 {
     public List<Sprite> levelSprite;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+ 
 }
